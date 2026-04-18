59a5a145f0c4c4614a4f7043d0788bb6f23eb371
Nicki Skipper <duckay@live.com>
2026-04-18 20:04:11 +0200

    Added xcode project

---
+++ the-dictator/the-dictator/the_dictatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_dictatorApp.swift
+//  the-dictator
+//
+//  Created by Nicki Skipper on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct the_dictatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
